64a7a2682a6605297f49fbc76e6dd8ba4abb5ff1
istott <isaac.cloyd@gmail.com>
2021-07-05 22:44:34 -0600

    created a fun app where I randomly grab a name

---
+++ random-name/src/App.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+
+const students = ['joe ', 'bob ', 'billy ', 'isaac '];
+
+function App() {
+  const [names, setNames] = useState(students);
+  const [randomName, setRandomName] = useState('')
+  const min = 0;
+  const max = names.length;
+
+  const buttonHandler = () => {
+    const getName = names[Math.floor(Math.random() * (max - min) + min)];
+
+    setRandomName(getName)
+  }
+
+  // useEffect(() => {
+  //   buttonHandler()
+  // })
+ 
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{randomName}</p>
+        <button onClick={buttonHandler}>get name</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
