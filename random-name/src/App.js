fd0c544baca467ad0bf1ceb5408a6e9bd11fe35a
istott <isaac.cloyd@gmail.com>
2021-07-13 21:21:30 -0600

    created mutlpe classes with their own list of students.
    picker can now select the class to randomly get name from.

--- random-name/src/App.js
+++ random-name/src/App.js
@@ -2,31 +2,59 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 
-const students = ['joe ', 'bob ', 'billy ', 'isaac '];
+const students = [
+  {
+    id: 1,
+    class:'math',
+    studentNames: ['joe ', 'bob ', 'billy ', 'isaac '],
+  },
+  {
+    id: 2,
+    class: 'science',
+    studentNames: ['fred', 'chris', 'zack', 'shawn', 'joshua', 'amy'],
+  }
+];
 
 function App() {
   const [names, setNames] = useState(students);
-  const [randomName, setRandomName] = useState('')
+  const [randomName, setRandomName] = useState('');
+  const [selectedClass, setSelectedClass] = useState(1);
   const min = 0;
-  const max = names.length;
+  const max = names[Number(selectedClass - 1)].studentNames.length;
 
-  const buttonHandler = () => {
-    const getName = names[Math.floor(Math.random() * (max - min) + min)];
+  const getRandomName = () => {
+    const randomNumber = Math.floor(Math.random() * (max - min) + min);
+    const getName = names[Number(selectedClass - 1)].studentNames[randomNumber];
+    setRandomName(getName);
+  };
 
-    setRandomName(getName)
+  const classHandler = (e) => {
+    setSelectedClass(e.target.value)
+    
   }
-
-  useEffect(() => {
-    buttonHandler()
-  })
- 
+  console.log(selectedClass)
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{randomName}</p>
-        <button onClick={buttonHandler}>get name</button>
+        <span>Green Valley Ranch High School</span>
+        <button>➕ student</button>
       </header>
+      <main className='main'>
+          <label for='class'>choose class </label>
+          <select
+            defaultValue={selectedClass}
+            onChange={classHandler}
+          >
+            <option value='1'>math</option>
+            <option value='2'>science</option>
+          </select>
+        <p>random name: {randomName}</p>
+        <button onClick={getRandomName} >click me</button>
+      </main>
+      <footer className='footer'>
+        <p>🎶 Im a school yo</p>
+      </footer>
     </div>
   );
 }
